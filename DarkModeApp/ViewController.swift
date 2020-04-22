9a5e00a560ef385923e9fb4f142e1e56a403a0e1
Yunus MAKAN <makanyunus@gmail.com>
2020-04-22 15:39:33 +0300

    Initial Commit

---
+++ DarkModeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkModeApp
+//
+//  Created by MAKAN on 22.04.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
